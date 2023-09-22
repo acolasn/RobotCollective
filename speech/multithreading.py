6a5edba95c75f56fc40efd7c0ae71f0bce49fa33
acolasn <anconi.1999@gmail.com>
2023-09-22 15:04:58 +0100

    Beggining of threading

--- speech/multithreading.py
+++ speech/multithreading.py
@@ -2,14 +2,18 @@ import threading
 import queue
 import chat_commanding
 import chat_driving
-
+import QR.capture_stream_with_downsampling_and_read_qr.
 
 # Create a new queue
-data_queue = queue.Queue()
+data_queue_motor = queue.Queue()
+data_queue_qr = queue.Queue()
+
 
 # Create two threads
 t1 = threading.Thread(target= chat_commanding.openAI_driver, args=(data_queue,))
 t2 = threading.Thread(target= chat_driving.chat_driver, args=(data_queue,))
+t2 = threading.Thread(target= chat_driving.chat_driver, args=(data_queue,))
+
 
 # Start the threads
 t1.start()
