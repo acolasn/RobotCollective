2a620bc00b046d9712a86c2e9e3463b20bc6cdf1
acolasn <anconi.1999@gmail.com>
2023-09-26 14:13:22 +0100

    It kinda works so far

--- speech/multithreading.py
+++ speech/multithreading.py
@@ -2,17 +2,14 @@ import threading
 import queue
 import chat_commanding
 import chat_driving
-import QR.capture_stream_with_downsampling_and_read_qr.
 
 # Create a new queue
 data_queue_motor = queue.Queue()
-data_queue_qr = queue.Queue()
 
 
 # Create two threads
-t1 = threading.Thread(target= chat_commanding.openAI_driver, args=(data_queue,))
-t2 = threading.Thread(target= chat_driving.chat_driver, args=(data_queue,))
-t2 = threading.Thread(target= chat_driving.chat_driver, args=(data_queue,))
+t1 = threading.Thread(target= chat_commanding.openAI_driver, args=(data_queue_motor,))
+t2 = threading.Thread(target= chat_driving.chat_driver, args=(data_queue_motor,))
 
 
 # Start the threads
